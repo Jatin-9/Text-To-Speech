ff4ed7532836647086841cf8b1667c0202c46dc5
Jatin Kandpal <jxk217@student.bham.ac.uk>
2023-08-17 18:07:45 +0100

    modified a and n sound , also added new rules in eng_aliasrules.hsp and eng_phones.hsp

--- haspell-files/src/Main.hs
+++ haspell-files/src/Main.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main = do
   let languageId = "eng"  -- assuming English language
   let voiceName = "voice"  -- assuming a 'default' voice
-  let inputText = " echo"
+  let inputText = "cent"
   -- Convert the input text to phonemes
   phonemes <- phonemize languageId inputText
   --Speak the phonemes
