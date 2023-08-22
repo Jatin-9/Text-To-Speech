66dabfcaa436e2e80935ad39c07d9b9bbc537ce9
Jatin Kandpal <jxk217@student.bham.ac.uk>
2023-08-22 21:15:36 +0100

    added new rules for silent E and also recorded some voice which are blue,have,page and mouse to prove that the rules work

--- haspell-files/src/Main.hs
+++ haspell-files/src/Main.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main = do
   let languageId = "eng"  -- assuming English language
   let voiceName = "voice"  -- assuming a 'default' voice
-  let inputText = "table"
+  let inputText = "blue"
   -- Convert the input text to phonemes
   phonemes <- phonemize languageId inputText
   --Speak the phonemes
